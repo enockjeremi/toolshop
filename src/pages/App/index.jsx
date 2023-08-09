d1285c08379053158ad9e74dfe286653892ec431
Enock Jeremi <enockjeremi@gmail.com>
2023-08-08 23:04:00 -0400

    estructura de pages

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Home from '../Home'
+import Account from '../Account'
+import Orders from '../Orders'
+import Order from '../Order'
+import NotFound from '../NotFound'
+import SingIn from '../SignIn'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <Account />
+      <Orders />
+      <Order />
+      <NotFound />
+      <SingIn />
+    </>
+  )
+}
+
+export default App
